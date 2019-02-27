c649eae9fc72d198cd19197ba08beb75a65544ba
unknown <student@cs.colman.ac.il>
2019-02-27 23:02:55 +0200

    feat (record): voice start and clear pad commands

--- move-to-vue2-fifth-exrc/src/components/record.vue
+++ move-to-vue2-fifth-exrc/src/components/record.vue
@@ -2,11 +2,19 @@
     <div>
         <div class="input-single">
             <textarea id="note
-            -textarea" placeholder="Create a new note by typing or using voice recognition." rows="6" v-model="noteTextarea"></textarea>
+            -textarea" placeholder="Here will be your recorded note" rows="6" v-model="noteTextarea"></textarea>
         </div>         
-        <button id="start-record-btn" title="Start Recording" @click="startRec">Start Recognition</button>
-        <button id="pause-record-btn" title="Pause Recording" @click="stopRec">Pause Recognition</button>
-        <button id="save-note-btn" title="Save Note" @click="clear">Clear pad </button>
+        <div v-if="isRecording == false">
+        <button id="start-record-btn" @click="startRec">Start</button>
+        <button id="save-note-btn" @click="clear">Clear</button>
+
+        </div>
+        <div v-else>
+        <button id="pause-record-btn" @click="stopRec">Pause</button>
+        <button id="save-note-btn" @click="clear">Clear</button>
+
+        </div>
+
         <p id="recording-instructions">{{recordingStatus}}</p>
     </div>
 </template>
@@ -20,14 +28,34 @@ export default {
         recognition:'',
         noteContent:'',
         noteTextarea:'',
-        recordingStatus:"Press the Start Recognition button and allow access",
-        isRecording: true,
+        recordingStatus:"Press the start button to start recording",
+        isRecording: false,
       }
   },
   created() {
+
+    if (annyang) {
+    // Let's define our first command. First the text we expect, and then the function it should call
+    var commands = {
+        'start': () => {
+        this.startRec();
+        },
+        'clear': () => {
+        this.clear();
+        },
+    };
+
+    // Add our commands to annyang
+    annyang.addCommands(commands);
+
+    // Start listening. You can call this here, or attach this call to an event, button, etc.
+    annyang.start();
+    }
+//-----------------------------------------------
     this.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
     this.recognition = new SpeechRecognition();
     this.recognition.addEventListener('result', (event) => {
+        this.isRecording = false;
         var current = event.resultIndex;
         // Get a transcript of what was said.
         var transcript = event.results[current][0].transcript;
@@ -35,28 +63,31 @@ export default {
         console.log(transcript);
         this.noteContent += transcript;
         this.noteTextarea = this.noteContent;
+        this.recordingStatus = "Finish recording";
+        annyang.start();
     });
   },
   methods: {
       startRec() {
+        annyang.abort();
         if (this.noteContent.length) {
             this.noteContent += ' ';
         }
         console.log("start");
         this.recognition.start();
+        this.isRecording = true;
         this.recordingStatus = "Recording...";
       },
       stopRec() {
         console.log("stop");
         this.recognition.stop();
         this.recordingStatus = "Finish recording";
-
+        this.isRecording = false;
       },
       clear() {
           this.noteContent = '';
           this.noteTextarea = '';
-          this.recordingStatus = "Press the Start Recognition button and allow access";
-
+          this.recordingStatus = "Press the start button to start recording";
       },
   },
 
