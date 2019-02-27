ea74d15bc8825e11319add84769b45b2fccda588
unknown <student@cs.colman.ac.il>
2019-02-27 13:36:52 +0200

    feat(imageDivider): rowDivider function was added

--- imageDivider.py
+++ imageDivider.py
@@ -5,10 +5,23 @@ def divider(pixMat):
 
 
 def rowDivider(pixMat):
-    return ""
+    is_row_started = False
+    rows = []
+    rowbeginning = None
+    for i in range(len(pixMat)):
+        for j in range(len(pixMat)) and is_row_started is False:
+            if pixMat[i][j] == 0:
+                is_row_started = True
+                rowbeginning = i
 
-def letterDivder(pixMat,startRow,endRow)
-    return ""
+        if is_row_started is False and c is not None:
+            rows.append({begin: rowbeginning, end: i-1})
+            rowbeginning = None
+        is_row_started = False
 
+    return rows
+
+def letterDivder(pixMat,startRow,endRow):
+    return ""
 
 
