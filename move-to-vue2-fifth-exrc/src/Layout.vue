0f71a8f96a3a6885518bbfcf5b9a39e953a9d0dc
unknown <student@cs.colman.ac.il>
2019-02-28 03:37:04 +0200

    feat (layer): adding nice css covering and moving through componnents

--- move-to-vue2-fifth-exrc/src/Layout.vue
+++ move-to-vue2-fifth-exrc/src/Layout.vue
@@ -1,30 +1,42 @@
 <template>
   <div id="app">
-        <div class="container">
-            <div id="flow">
-                <span class="flow-1"></span>
-                <span class="flow-2"></span>
-                <span class="flow-3"></span>
-            </div>
-        </div>
-
-        <navbar></navbar>
-
-        <div class="home-columns columns">
-            <div class="column is-half is-home-left-column" style="background-color:lightgray">
-                <label v-if="!isRecord" @click="isRecord=true"><i class="fa fa-microphone"></i></label>
-                <record v-else></record>
-            </div>
-            <div class="column is-home-right-column" style="background-color:lightgray">
-                <label v-if="!isUpload" @click="isUpload=true"><i class="fa fa-image"></i></label>
-            </div>
-        </div>
-
-
-        <div class="container">
-            
-        </div>
-
+            <div class="ct" id="t1">
+                <div class="ct" id="t2">
+                  <div class="ct" id="t3">
+                    <div class="ct" id="t4">
+                        <ul id="menu">
+                            <a href="#t1"><img src="./img/logo.png"></a><br>
+                            <a href="#t2"><img src="./img/P2T.png"></a><br>
+                            <a href="#t3"><img src="./img/S2t.png"></a><br>
+                            <a href="#t4"><img src="./img/logo.png"></a><br>
+                        </ul>
+                        <div class="page" id="p1">
+                                <div class="container">
+                                        <div id="flow">
+                                            <span class="flow-1"></span>
+                                            <span class="flow-2"></span>
+                                            <span class="flow-3"></span>
+                                        </div>
+                                    </div>
+                           <section class="icon fa fa-bolt"><span class="title">Bolt</span><span class="hint">Like this pen to see the magic!...<br> Just kidding, it won't happen anything but I'll be really happy If you do so.</span></section>  
+                        </div>
+                        <div class="page" id="p2">
+                          <section class="icon fa fa-keyboard-o"><span class="title">Type</span></section>
+                        </div>  
+                        <div class="page" id="p3">
+                          <record></record>
+                        </div>
+                        <div class="page" id="p4">
+                            <div class="container">
+                                <h1>About us</h1>
+                                <p>text text... blah blah blah</p>
+                            </div>
+                        </div> 
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
   </div>
 </template>
 
@@ -46,6 +58,11 @@ export default {
 }
 </script>
 <style>
+img {
+    height: 90px;
+    width: 90px;
+}
+
 .column {
     height: 900px!important;
 }
@@ -54,32 +71,204 @@ label {
     margin-top: 250px;
     margin-left: 350px;
 }
-/*
-.card {overflow:hidden}
-.card.large {
-height: 600px;
--webkit-backface-visibility: hidden;
-backface-visibility: initial;
-border-radius: 5px;
+html, body, .page {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  transition: all .6s cubic-bezier(.5, .2, .2, 1.1);
+  -webkit-transition: all .6s cubic-bezier(.5, .2, .2, 1.1);
+  -moz-transition: all .6s cubic-bezier(.5, .2, .2, 1.1);
+  -o-transition: all .6s cubic-bezier(.5, .2, .2, 1.1);  
+  color: #fff;
+  overflow: hidden; 
 }
+
+* {
+  font-family: 'open sans', 'lato', 'helvetica', sans-serif;
+}
+
+.page {
+  position: absolute;
+}
+
+#p1 {
+  left: 0;
+}
+
+#p2, #p3, #p4, #p5 {
+  left: 200%;
+}
+
+#p1 { background: black; }
+#p2 { background: rgb(45, 180, 117); }
+#p3 { background: rgb(77, 53, 121); }
+#p4 { background: darksalmon; }
+
+#t2:target #p2,
+#t3:target #p3,
+#t4:target #p4,
+#t5:target #p5 {
+  transform: translateX(-190%);
+  -webkit-transform: translateX(-190%);
+  -moz-transform: translateX(-190%);
+  -o-transform: translateX(-190%);
+  transition-delay: .4s !important;
+}
+
+#t2:target #p1, 
+#t3:target #p1,
+#t4:target #p1,
+#t5:target #p1{
+  background: black;
+}
+
+#t2:target #p1 .icon, 
+#t3:target #p1 .icon,
+#t4:target #p1 .icon,
+#t5:target #p1 .icon {
+  -webkit-filter: blur(3px);
+}
+
+.icon {
+  color: #fff;
+  display: block;
+}
+
+ul img:hover {
+  opacity: 0.5;
+}
+
+.page .icon .title {
+  line-height: 2;
+}
+
+#t2:target ul .icon,
+#t3:target ul .icon,
+#t4:target ul .icon,
+#t5:target ul .icon{
+  transform: scale(.6);
+  -webkit-transform: scale(.6);
+  -moz-transform: scale(.6);
+  -o-transform: scale(.6);
+  transition-delay: .25s;
+}
+
+#t2:target #dos,
+#t3:target #tres,
+#t4:target #cuatro,
+#t4:target #cinco {
+  transform: scale(1.2) !important;
+  -webkit-transform: scale(1.2) !important;
+  -moz-transform: scale(1.2) !important;
+  -o-transform: scale(1.2) !important;
+}
+
+ul {
+  position: fixed;
+  z-index: 1;
+  top: 150px;
+  left: 0;
+  margin: auto;
+  height: 280px;
+  width: 10%;
+  padding: 0;
+  text-align: center;
+ }
+
+a {
+  text-decoration: none;
+}
+
+.title, .hint {
+  display: block;
+}
+
+.title {
+  font-size: 38px;
+}
+
+.hint {
+  font-size: 13px;
+}
+
+#p4 .hint {
+  display: inherit !important;
+}
+
+.hint a {
+  color: yellow;
+  transition: all 250ms ease-out;
+  -webkit-transition: all 250ms ease-out;
+  -moz-transition: all 250ms ease-out;
+  -o-transition: all 250ms ease-out;
+}
+
+.hint a:hover {
+  color: #FFF;
+}
+
+.line-trough {
+  text-decoration: line-through;
+}
+
+.page .icon {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 10%;
+  left: 0;
+  width: 270px;
+  height: 170px;
+  margin: auto;
+  text-align: center;
+  font-size: 80px;
+  line-height: 1.3;
+  transform: translateX(360%);
+  -webkit-transform: translateX(360%);
+  -moz-transform: translateX(360%);
+  -o-transform: translateX(360%);
+  transition: all .5s cubic-bezier(.25, 1, .5, 1.25);
+  -webkit-transition: all .5s cubic-bezier(.25, 1, .5, 1.25);
+  -moz-transition: all .5s cubic-bezier(.25, 1, .5, 1.25);
+  -o-transition: all .5s cubic-bezier(.25, 1, .5, 1.25);
+}
+
+.page#p1 .icon {
+  height: 100px;
+}
+
+.page#p1 .icon {
+  transform: translateX(10%) !important;
+}
+
+#t2:target .page#p2 .icon,
+#t3:target .page#p3 .icon,
+#t4:target .page#p4 .icon,
+#t5:target .page#p5 .icon {
+  transform: translateX(0) !important;
+  -webkit-transform: translateX(0) !important;
+  -moz-transform: translateX(0) !important;
+  -o-transform: translateX(0) !important;
+  transition-delay: 1s;
+}
+
 .media-content {
 overflow: hidden;
 }
-.title.no-padding {
-margin-bottom: 0 !important;
-}
+
 #flow span {
 display: block;
 width: 200vw;
 height: 200vw;
 position: absolute;
-top: -180vw;
+top: -185vw;
 left: -50vw;
 border-radius: 90vw;
 opacity: 0.6;
 }
 .flow-1 {
-background: #3281ff;
+background: rgb(77, 53, 121);
 -webkit-animation: rotating 20s linear infinite;
 -moz-animation: rotating 20s linear infinite;
 -ms-animation: rotating 20s linear infinite;
@@ -87,7 +276,7 @@ background: #3281ff;
 animation: rotating 20s linear infinite;
 }
 .flow-2 {
-background: #f442ee;
+background: darkorange;
 position: absolute;
 -webkit-animation: rotating 15s linear infinite;
 -moz-animation: rotating 15s linear infinite;
@@ -96,7 +285,7 @@ position: absolute;
 animation: rotating 15s linear infinite;
 }
 .flow-3 {
-background: #42eef4;
+background: cyan;
 position: absolute;
 -webkit-animation: rotating 7s linear infinite;
 -moz-animation: rotating 7s linear infinite;
@@ -136,24 +325,5 @@ to {
 transform: rotate(360deg);
 }
 }
-.rotating {
--webkit-animation: rotating 2s linear infinite;
--moz-animation: rotating 2s linear infinite;
--ms-animation: rotating 2s linear infinite;
--o-animation: rotating 2s linear infinite;
-animation: rotating 2s linear infinite;
-}
-.footer{
-background-color: #222831;
-color: lemonchiffon;
-}
-.footer p, strong
-{color: lemonchiffon}
-.footer a:hover
-{color: crimson;}
-.fa
-{color: lemonchiffon;
-    margin: 10px} */
-
 </style>
 
